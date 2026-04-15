db50abaa981c57fad8191afdb04dcaecaefd6954
Sala K 405 <salak405@salasfr.itm.edu.co>
2026-04-15 09:10:44 -0500

    Metodos

--- Metodos.java
+++ Metodos.java
@@ -1,3 +1,47 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class Metodos {
 
+    public LinkedList<ObjEstudiante> LLenarLista(LinkedList<ObjEstudiante> l, Scanner sc) {
+
+        boolean pedir = true;
+
+        while (pedir) {
+
+            ObjEstudiante o = new ObjEstudiante();
+
+            System.out.println("Ingrese el nombre del estudiante");
+            o.setNombre(sc.next());
+
+            System.out.println("Ingrese el carnet");
+            o.setCarnet(sc.next());
+
+            System.out.println("Ingrese la cédula");
+            o.setCedula(sc.nextInt());
+
+            l.add(o);
+            System.out.println("Desea seguir Ingresando 1) Si , 2) No");
+
+            int opt = sc.nextInt();
+
+            if (opt == 2) {
+
+                pedir = false;
+            }
+        }
+        return l;
+    }
+
+    public void MostrarLista(LinkedList<ObjEstudiante> l)
+    {
+        for (ObjEstudiante o : l) {
+            System.out.println("Nombre: " + o.getNombre());
+            System.out.println("Carnet: " + o.getCarnet());
+            System.out.println("Cédula: " + o.getCedula());
+            System.out.println("--------------------------------------");
+            System.out.println();
+        }
+    }
 }
+
